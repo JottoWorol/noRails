7832745f99b7132293164928ffb7e7d8df3fcc21
JottoWorol <m.poltaryihin@innopolis.university>
2019-09-21 19:03:28 +0300

    Update spritesheet.lua

--- spritesheet.lua
+++ spritesheet.lua
@@ -7,42 +7,39 @@ sheetOptions =
         { ---1) Tree
             x = 0,
             y = 0,
-            --width = 335,
-            width = 375,  --задевает корову, поэтому сделал ближе к квадрату
-            height = 403
+            width = 395,
+            height = 395
         },
         { ---2) Cow
-            x = 335,
+            x = 380,
             y = 0,
-            --width = 301,
-            --height = 403
-            width = 403,
-            height = 403
+            width = 395,
+            height = 395
         },
         {
           ---3) Lake
-              x = 0,
-              y = 403,
-              width = 427,
-              height = 621
+              x = 1641,
+              y = 0,
+              width = 407,
+              height = 604
         },
         { ---4) train-2
-            x = 335+301,
-            y = 356,
+            x = 938,
+            y = 348,
             width = 256,
             height = 320
         },
         { ---5) train-1
-            x = 335+301,
-            y = 0,
-            width = 256,
-            height = 356
+            x = 938,
+            y = 11,
+            width = 339,
+            height = 339
         },
         { ---6) rails
-            x = 904 - 222,
-            y = 1055 - 340,
-            width = 222,
-            height = 340
+            x = 11,
+            y = 670,
+            width = 301,
+            height = 301
         }
     },
 }
