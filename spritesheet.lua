76ee2156aca651c470a83fa3951e71dcdc003cf4
JottoWorol <m.poltaryihin@innopolis.university>
2019-10-17 21:47:42 +0300

    Блоки - ID

--- spritesheet.lua
+++ spritesheet.lua
@@ -28,48 +28,48 @@ sheetOptions =
             width = 826,
             height = 413
         },
-        { ---5) Tree
+        { ---5) Coal
+            x = 908,
+            y = 674,
+            width = 410,
+            height = 408
+        },
+        { ---6) Tree
             x = 0,
             y = 0,
             width = 395,
             height = 395
         },
-        { ---6) Cow
+        { ---7) Cow
             x = 416,
             y = 0,
             width = 395,
             height = 395
         },
         {
-          ---7) House1
-              x = 1608,
-              y = 0,
-              width = 395,
-              height = 395
+          ---8) House1
+            x = 1608,
+            y = 0,
+            width = 395,
+            height = 395
         },
-        { ---8) House2
+        { ---9) House2
             x = 33,
             y = 1237,
             width = 347,
             height = 347
         },
-        { ---9) YellowGrass
+        { ---10) YellowGrass
             x = 560,
             y = 720,
             width = 232,
             height = 232
         },
-        { ---10) Lake
+        { ---11) Lake
             x = 1668,
             y = 542,
             width = 354,
             height = 604
-        },
-        { ---11) Coal
-            x = 908,
-            y = 674,
-            width = 410,
-            height = 408
         }
     }
 }
