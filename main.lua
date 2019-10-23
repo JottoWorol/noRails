6159b5e2141f3447c3c64fec6f2b570de440fc34
JottoWorol <m.poltaryihin@innopolis.university>
2019-10-23 03:16:28 +0300

    Прежний фпс(

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 ------ GLOBAL Settings ------
 _W = display.actualContentWidth  --ширина экрана
 _H = display.actualContentHeight --высота экрана
-display.fps = 30
+display.fps = 60
 
 --левый нижний угол
 bottomY = display.contentCenterY+_H*0.5
